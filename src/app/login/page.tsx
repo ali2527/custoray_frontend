86de2f742abbc96e0fd66b182e369361fee6f8bb
ali2527 <ali.talha3357@gmail.com>
2025-07-02 02:25:58 +0500

    login page setup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { LoginForm } from "@/components/login-form"
+import { ToggleButton } from "@/components/ui/toggleButton"
 
-function login() {
+export default function Page() {
   return (
-    <div>
-      
+    <div className="flex min-h-svh w-full items-center justify-center px-0 py-4 md:p-10">
+      <ToggleButton />
+      <div className="w-full max-w-4xl">
+        <LoginForm />
+      </div>
     </div>
   )
 }
-
-export default login
